632b4dda1a0bf1eab60f975db6a4f0d8f53d7602
joonghyun <joongh@ewhain.net>
2021-10-08 00:54:53 +0900

    ⚗️ practice

---
+++ vanilla-js-ChromeWeb/김중현/basics/app.js
@@ -0,0 +1,38 @@
+const h1 = document.querySelector('.hello h1');
+
+function handleTitleClick() {
+   h1.style.color = 'blue';
+}
+
+function handleMouseEnter() {
+   h1.innerText = 'Mouse is here!';
+}
+
+function handleMouseLeave() {
+   h1.innerText = 'Mouse is gone!';
+}
+
+function handleWindowResize() {
+   document.body.style.backgroundColor = 'gray';
+}
+
+function handleWindowCopy() {
+   alert('copier!');
+}
+
+function handleWindowOffline() {
+   alert('SOS NO WIFI!');
+}
+
+function handleWindowOnline() {
+   alert('ALL GOOOD!');
+}
+
+h1.addEventListener('click', handleTitleClick);
+h1.addEventListener('mouseenter', handleMouseEnter);
+h1.addEventListener('mouseleave', handleMouseLeave);
+
+window.addEventListener('resize', handleWindowResize);
+window.addEventListener('copy', handleWindowCopy);
+window.addEventListener('offline', handleWindowOffline);
+window.addEventListener('online', handleWindowOnline);
