57b8823bdf5a13587c5dab0b7094a88b6fe29881
ssini <cdnnnl@ewhain.net>
2021-10-11 20:16:21 +0900

    [GDSC FE Study {7}] 장효신 add geolocation weather view

---
+++ vanilla-js-ChromeWeb/장효신/js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "f4748de4e40229f61afa3e3a67d443bc";
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    // JS가 url 부름
+    fetch(url).then(response => response.json())
+    .then(data=>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.")
+}
+// 현재 위치
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
