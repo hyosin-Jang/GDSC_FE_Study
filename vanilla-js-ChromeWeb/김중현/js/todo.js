05eeeec60c6454a83063731171b29e64bab35645
joonghyun <joongh@ewhain.net>
2021-11-04 13:52:46 +0900

    ⚡️ Improve loading todos feature

--- vanilla-js-ChromeWeb/김중현/js/todo.js
+++ vanilla-js-ChromeWeb/김중현/js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('#todo-form input');
 const toDoList = document.getElementById('todo-list');
 
-const toDos = [];
+const TODOS_KEY = 'todos';
+
+let toDos = [];
 
 function saveToDos() {
-   localStorage.setItem('todos', toDos);
+   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
@@ -34,3 +36,11 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+   const parsedToDos = JSON.parse(savedToDos);
+   toDos = parsedToDos;
+   parsedToDos.forEach(paintToDo);
+}
