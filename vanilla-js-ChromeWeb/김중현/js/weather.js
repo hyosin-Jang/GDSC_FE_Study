9ffbc9da04bc92dcb6656274e1ee36178d6c39b6
joonghyun <joongh@ewhain.net>
2021-11-04 15:33:02 +0900

    ✨ Add weather API

---
+++ vanilla-js-ChromeWeb/김중현/js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = 'e7b1452f9c9099d75fec2cb4fa923e6d';
+
+function onGeoOk(position) {
+   const lat = position.coords.latitude;
+   const lon = position.coords.longitude;
+   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+   fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+         const weather = document.querySelector('#weather span:first-child');
+         const city = document.querySelector('#weather span:last-child');
+         city.innerText = data.name;
+         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      });
+}
+function onGeoError() {
+   alert("Can't find you! No weather for you,,,");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
